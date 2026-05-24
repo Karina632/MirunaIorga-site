b65b3125138e9d8427f6f2c5a80ea1871c01cf03
Karina632 <karinaceaunel@gmail.com>
2026-05-24 15:25:22 +0300

    last update bf send

--- js/script.js
+++ js/script.js
@@ -124,3 +124,28 @@ if (container) {
 
   updateDots();
 }
+
+
+/* ================= FAQ ================= */
+
+const faqItems = document.querySelectorAll(".faq-item");
+
+faqItems.forEach(item => {
+
+  const question = item.querySelector(".faq-question");
+
+  question.addEventListener("click", () => {
+
+    const isActive = item.classList.contains("active");
+
+    faqItems.forEach(i => {
+      i.classList.remove("active");
+    });
+
+    if (!isActive) {
+      item.classList.add("active");
+    }
+
+  });
+
+});
